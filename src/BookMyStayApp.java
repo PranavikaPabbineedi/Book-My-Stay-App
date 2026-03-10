f5768150ad9de6b0e86aa20e5ab0d9d43c3bb073
Pranavika Pabbineedi <pp2737@srmist.edu.in>
2026-03-10 15:17:33 +0530

    UseCase1: Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,12 @@
 public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("        Book My Stay Application       ");
+        System.out.println("     Hotel Booking Management System   ");
+        System.out.println("             Version 1.0               ");
+        System.out.println("=======================================");
+        System.out.println("Application started successfully.");
+        System.out.println("Ready for further operations...");
+    }
 }
